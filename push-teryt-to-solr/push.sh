a57a5ed10fb41142c5bd1bedb52b372d5b592b95
W <github@vink.pl>
2016-01-21 22:26:07 +0100

    update to Solr 5

--- push-teryt-to-solr/push.sh
+++ push-teryt-to-solr/push.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl 'http://liferay.lan:8983/solr/teryt/update?commit=true' --data-binary @terc.json -H 'Content-type:application/json'
+curl 'http://osm-teryt-solr.lan:8983/solr/teryt/update?commit=true' --data-binary @terc.json -H 'Content-type:application/json'
